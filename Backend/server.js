4c50ff7bf8670a508aafd16d2a35fd2c7852b294
muthupandy <vikraam17@gmail.com>
2025-07-25 20:58:00 +0530

    backend server connected with backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,37 @@
 const express = require('express');
-
+const mongoose = require('mongoose')
 const app = express();
 
+const User = require('./model/user')
+
+PORT = 3000;
+app.use(express.json())
+mongoose.connect('mongodb://localhost:27017/user')
+.then(()=>console.log("mongoDB connected✨"))
+.catch((err)=>console.log(err))
+
+
 app.get('/',(req,res)=>{
-    res.send('Hello from skill maven')
+    res.send("Get route is working")
 })
 
-app.listen(8000,()=>{
-    console.log(`Server running in http://localhost:${8000}`)
+app.post('/create', async(req,res)=>{
+    const user = new User(req.body);
+    const success = await user.save();
+    if(success)
+        res.send('User created Successfully')
+    else 
+        res.send('User not Created')
+})
+
+app.put('/',(req,res)=>{
+    res.json({message:'put route is working'})
+})
+
+app.delete('/',(req,res)=>{
+    res.json({message:"delete route is working"})
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server running in http://localhost:${PORT}`)
 })
