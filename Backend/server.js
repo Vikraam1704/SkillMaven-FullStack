08ba874810497a10dce7d3c4c7b537afec68ec30
vikraam17 <vikraam17@gmail.com>
2025-07-31 20:17:37 +0530

    dotenv file added

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
-PORT = 3000;
-app.use(express.json())
 const connectDB = require('./config/db')
 const cors = require('cors')
-
+const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
 const todoRouter = require('./routes/todoRoutes');
+dotenv.config();
 app.use(cors());
+PORT = process.env.PORT;
 connectDB();
-
+app.use(express.json())
 app.use('/',userRoutes)
 
 app.use('/todos',todoRouter)
